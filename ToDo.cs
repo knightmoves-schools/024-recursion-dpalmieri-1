be3f0928597d8ab7927332e244992115425e6a7f
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-09 12:00:46 -0600

    Fix order of operations in MarkAsDone method

--- ToDo.cs
+++ ToDo.cs
@@ -4,15 +4,16 @@ public class ToDo
     // Add your code here
     public int Count = 0;
     public string[] MarkAsDone(string[] todos){
-        Count++;
         if(todos.Length > Count){
             todos[Count] = "done - " + todos[Count];
-            return MarkAsDone(todos[Count]);
+            Count++;
+            return MarkAsDone(todos);
         } else {
             return todos;
         }
     }
 }
+
 
 
 
